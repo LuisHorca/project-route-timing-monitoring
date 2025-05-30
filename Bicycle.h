cb432d42c3866a7b084abfd47c10256282474eda
LuisHorca <a01713551@tec.mx>
2025-05-29 23:00:31 -0600

    Create Bicycle.h

---
+++ Bicycle.h
@@ -0,0 +1,18 @@
+#ifndef BICYCLE_H
+#define BICYCLE_H
+
+#include "Vehicle.h"
+using namespace std;
+
+class Bicycle : public Vehicle {
+    bool hasGears;
+    int currentGear;
+    bool lightsOn;
+public:
+    Bicycle(const string& brand, const string& model, bool hasGears);
+    double maxSpeed() const override;
+    void changeGear(int newGear);
+    bool areLightsOn() const;
+};
+
+#endif 
