fafe192a7c8f3789d53d8f5921d16d1ff3621365
LuisHorca <a01713551@tec.mx>
2025-05-29 23:02:55 -0600

    Create Route.h

---
+++ Route.h
@@ -0,0 +1,16 @@
+#ifndef ROUTE_H
+#define ROUTE_H
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Route {
+public:
+    static double getDistance(const string& origin, const string& destination);
+    static bool routeExists(const string& origin, const string& destination);
+    static vector<string> destinationsFrom(const string& origin);
+};
+
+#endif 
