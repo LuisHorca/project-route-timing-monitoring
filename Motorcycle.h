6502210a6cd9c55a905b0e6d80a694e243c88196
LuisHorca <a01713551@tec.mx>
2025-05-30 20:37:31 -0600

    Update Motorcycle.h

--- Motorcycle.h
+++ Motorcycle.h
@@ -8,6 +8,7 @@ class Motorcycle : public Vehicle {
     double engineCapacity;
 public:
     Motorcycle(const string& brand, const string& model, double engineCapacity);
+    string displayInfo() const override;
     double maxSpeed() const override;
 };
 
