d30fd05d0070ab66d249a492c7868a71d5f9876a
LuisHorca <a01713551@tec.mx>
2025-05-30 20:36:46 -0600

    Update Car.h

--- Car.h
+++ Car.h
@@ -8,9 +8,11 @@ class Car : public Vehicle {
     double horsepower;
 public:
     Car(const string& brand, const string& model, double horsepower);
+    string displayInfo() const override;
     double maxSpeed() const override;
-    double fuelConsumptionPerKm() const;
     double travelTime(double distance) const override;
+    double fuelConsumptionPerKm() const;
+    string displayFuelConsumption(double distance) const;
 };
 
 #endif 
