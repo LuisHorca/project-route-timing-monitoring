216dd87adefa5dbe7f7eda8743ad8aa9c168fd5e
LuisHorca <a01713551@tec.mx>
2025-06-07 16:31:45 -0600

    Update Route.h

--- Route.h
+++ Route.h
@@ -3,20 +3,26 @@
 
 #include <string>
 #include <vector>
-#include <unordered_map>
 using namespace std;
 
+// Manage list of locations and distances
 class Route {
+private:
+    vector<string> locations;           // city names
+    vector<vector<double>> distances;   // distance matrix
 public:
-    static vector<string> getAllOrigins();
+    Route(const vector<string>& initialLocations,
+          const vector<vector<double>>& initialDistances);
 
+    size_t getNumLocations() const;           // count
+    string getLocation(size_t idx) const;     // name at index
+    vector<string> getAllLocations() const;   // all names
+    double getDistance(size_t i, size_t j) const; // from matrix
+    vector<double> distancesFrom(size_t i) const; // row data
 
-    static vector<string> destinationsFrom(const string& origin);
-
-
-    static double getDistance(const string& origin, const string& destination);
-
-    static bool routeExists(const string& origin, const string& destination);
+    size_t addLocation(const string& name, const vector<double>& rowDistances);
+    bool removeLocation(size_t idx);
+    bool setDistance(size_t i, size_t j, double dist);
 };
 
 #endif 
