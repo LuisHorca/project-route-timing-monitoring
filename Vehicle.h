9acf62c57d33ea2a63a7e6b6d5b788c80d3383fa
LuisHorca <a01713551@tec.mx>
2025-06-07 16:26:33 -0600

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -2,21 +2,28 @@
 #define VEHICLE_H
 
 #include <string>
+#include <ostream>
 using namespace std;
 
+// Base class for all vehicles
 class Vehicle {
 protected:
-    string brand;
-    string model;
-    double currentSpeed;
+    string brand;        // vehicle brand
+    string model;        // vehicle model
+    double currentSpeed; // current speed in km/h
 public:
     Vehicle(const string& brand, const string& model);
     virtual ~Vehicle();
-    virtual string getIdentifier() const;
-    virtual string displayInfo() const = 0;
-    virtual double maxSpeed() const = 0;
-    virtual double travelTime(double distance) const;
-    virtual void accelerate(double amount);
+
+    virtual string getIdentifier() const;          // returns brand + model
+    virtual string displayInfo() const = 0;        // basic info string
+    virtual double maxSpeed() const = 0;           // max possible speed
+    virtual double travelTime(double distance) const; // compute travel time
+    virtual void accelerate(double amount);        // increase current speed
+
+    friend ostream& operator<<(ostream& os, const Vehicle& v); // print info
+    virtual bool operator==(const Vehicle& other) const;       // compare by id
+    virtual bool operator<(const Vehicle& other) const;        // order by id
 };
 
 #endif 
