15bc1023352149222c73989af48dc4b628daee80
LuisHorca <a01713551@tec.mx>
2025-06-07 20:51:01 -0600

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,40 +1,41 @@
 #include "Vehicle.h"
 #include "Route.h"
 
+// Base class constructor
 Vehicle::Vehicle(const string& brand, const string& model)
   : brand(brand), model(model), currentSpeed(0.0) {}
 
 Vehicle::~Vehicle() {}
 
+// Returns "brand model"
 string Vehicle::getIdentifier() const {
-    // Combine brand and model
     return brand + " " + model;
 }
 
+// Compute travel time (hours) given distance (km)
 double Vehicle::travelTime(double distance) const {
-    // time = distance / speed
     return distance / maxSpeed();
 }
 
+// Increase current speed by amount, capped at maxSpeed
 void Vehicle::accelerate(double amount) {
-    // increase speed but not above max
     currentSpeed += amount;
     if (currentSpeed > maxSpeed()) currentSpeed = maxSpeed();
 }
 
+// Stream-insertion operator uses displayInfo()
 ostream& operator<<(ostream& os, const Vehicle& v) {
-    // use displayInfo for output
     os << v.displayInfo();
     return os;
 }
 
+// Compare two vehicles by brand and model
 bool Vehicle::operator==(const Vehicle& other) const {
-    // equal if same brand and model
     return brand == other.brand && model == other.model;
 }
 
+// Order vehicles by brand then model
 bool Vehicle::operator<(const Vehicle& other) const {
-    // compare by brand then model
     if (brand != other.brand) return brand < other.brand;
     return model < other.model;
 }
