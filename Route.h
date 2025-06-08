ec6d5b142e0b3ad3b10438b7ada0f1284eee0c66
LuisHorca <a01713551@tec.mx>
2025-06-07 20:48:50 -0600

    Update Route.h

--- Route.h
+++ Route.h
@@ -5,25 +5,25 @@
 #include <vector>
 using namespace std;
 
-// Manage list of locations and distances
+// Manages list of locations and distances
 class Route {
 private:
-    vector<string> locations;           // city names
-    vector<vector<double>> distances;   // distance matrix
+    vector<string> locations;         // city names
+    vector<vector<double>> distances; // distances[i][j] = km between
 public:
     Route(const vector<string>& initialLocations,
           const vector<vector<double>>& initialDistances);
 
-    size_t getNumLocations() const;           // count
-    string getLocation(size_t idx) const;     // name at index
-    vector<string> getAllLocations() const;   // all names
-    double getDistance(size_t i, size_t j) const; // from matrix
-    vector<double> distancesFrom(size_t i) const; // row data
+    size_t getNumLocations() const;            // number of locations
+    string getLocation(size_t idx) const;      // name at index
+    vector<string> getAllLocations() const;    // all names
+    double getDistance(size_t i, size_t j) const; // lookup distance
+    vector<double> distancesFrom(size_t i) const;  // row data
 
-    size_t addLocation(const string& name, const vector<double>& rowDistances);
-    bool removeLocation(size_t idx);
-    bool setDistance(size_t i, size_t j, double dist);
+    size_t addLocation(const string& name,
+                       const vector<double>& rowDistances); // add new
+    bool removeLocation(size_t idx);                // remove by index
+    bool setDistance(size_t i, size_t j, double dist); // update both
 };
 
 #endif 
-
