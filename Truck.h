466928786138768facecebe7108c0d7ae0e9c9c3
LuisHorca <a01713551@tec.mx>
2025-06-07 22:08:42 -0600

    Update Truck.h

--- Truck.h
+++ Truck.h
@@ -4,19 +4,11 @@
 #include "Vehicle.h"
 using namespace std;
 
-// Truck subclass
+// Truck with fixed top speed
 class Truck : public Vehicle {
-    double capacity;    // max load in tons
-    double currentLoad; // current cargo
 public:
-    Truck(const string& brand, const string& model, double capacity);
-
-    string displayInfo() const override; // info for Truck
-    double maxSpeed() const override;    // Truck speed
-
-    bool canLoad(double weight) const;   // check capacity
-    void load(double weight);            // add cargo
-    void unload(double weight);          // remove cargo
+    Truck(const string& brand, const string& model);
+    double maxSpeed() const override; // override top speed
 };
 
-#endif 
+#endif // TRUCK_H
