53fbfdeca59b967e16b9e6ac6f8d8cb61ce081fa
LuisHorca <a01713551@tec.mx>
2025-06-07 20:51:38 -0600

    Update Bicycle.cpp

--- Bicycle.cpp
+++ Bicycle.cpp
@@ -1,27 +1,29 @@
 #include "Bicycle.h"
 #include <sstream>
 
+// Bicycle constructor
 Bicycle::Bicycle(const string& brand, const string& model, bool gears)
   : Vehicle(brand, model), hasGears(gears), currentGear(1), lightsOn(false) {}
 
+// Return descriptive string
 string Bicycle::displayInfo() const {
-    // Bicycle brand/model and gear
     ostringstream oss;
     oss << "Bicycle: " << brand << " " << model;
     if (hasGears) oss << ", Gear " << currentGear;
     return oss.str();
 }
 
+// Max speed depends on gears
 double Bicycle::maxSpeed() const {
-    // speed depends on gears
     return hasGears ? 30.0 : 15.0;
 }
 
+// Change gear within range
 void Bicycle::changeGear(int newGear) {
-    // valid gear range 1-10
     if (newGear >= 1 && newGear <= 10) currentGear = newGear;
 }
 
+// Check if lights are on
 bool Bicycle::areLightsOn() const {
     return lightsOn;
 }
