75e36ee4e3036c41d4c15ff00649d16116ef8d1c
LuisHorca <a01713551@tec.mx>
2025-06-07 22:12:22 -0600

    Update Motorcycle.cpp

--- Motorcycle.cpp
+++ Motorcycle.cpp
@@ -1,20 +1,11 @@
 #include "Motorcycle.h"
-#include <sstream>
 
-// Motorcycle constructor
 Motorcycle::Motorcycle(const string& brand, const string& model, double ec)
-  : Vehicle(brand, model), engineCapacity(ec) {}
+  : Vehicle(brand, model), engineCapacity(ec)
+{}
 
-// Return descriptive string
-string Motorcycle::displayInfo() const {
-    ostringstream oss;
-    oss << "Motorcycle: " << brand << " " << model
-        << ", " << engineCapacity << " cc";
-    return oss.str();
-}
-
-// Max speed in km/h
 double Motorcycle::maxSpeed() const {
-    return engineCapacity * 0.05;
+    return engineCapacity * 0.05; // km/h
 }
+
 
