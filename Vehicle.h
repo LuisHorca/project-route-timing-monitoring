fc2152ce80e691ed681cb72f855ffb253c559812
LuisHorca <a01713551@tec.mx>
2025-06-07 23:24:29 -0600

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -17,19 +17,20 @@ public:
     // Returns "brand model"
     string getIdentifier() const;
 
-    // Method overriding: subclasses define top speed
+    // Must be overridden by each subclass
     virtual double maxSpeed() const = 0;
 
-    // Method overriding: compute travel time in hours
+    // Compute travel time in hours
     virtual double travelTime(double distance) const;
 
-    // Method overloading: compute travel time in hours or minutes
+    // Overloaded: compute travel time in hours or minutes
     double travelTime(double distance, bool inMinutes) const;
 
-    // Operator overloading
+    // Operator overloads
     friend ostream& operator<<(ostream& os, const Vehicle& v);
     virtual bool operator==(const Vehicle& other) const;
     virtual bool operator<(const Vehicle& other) const;
 };
 
 #endif 
+
