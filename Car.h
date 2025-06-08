888dce1fc539d3dcedb8cf8983283ad16f053eee
LuisHorca <a01713551@tec.mx>
2025-06-07 20:47:41 -0600

    Update Car.h

--- Car.h
+++ Car.h
@@ -12,12 +12,12 @@ public:
 
     string displayInfo() const override;               // info for Car
     double maxSpeed() const override;                  // Car max speed
-    double travelTime(double distance) const override; // override travelTime
+    double travelTime(double distance) const override; // overridden travelTime
 
     double fuelConsumptionPerKm() const;               // fuel use per km
     string displayFuelConsumption(double distance) const; // fuel for distance
 
-    bool operator==(const Vehicle& other) const override; // compare Cars
+    bool operator==(const Vehicle& other) const override; // Car equality
 };
 
 #endif 
