41cf61a2f94a47ca7d607ae28710e0d0652a82b1
LuisHorca <a01713551@tec.mx>
2025-06-07 20:51:51 -0600

    Update Truck.cpp

--- Truck.cpp
+++ Truck.cpp
@@ -1,30 +1,34 @@
 #include "Truck.h"
 #include <sstream>
 
+// Truck constructor
 Truck::Truck(const string& brand, const string& model, double cap)
   : Vehicle(brand, model), capacity(cap), currentLoad(0.0) {}
 
+// Return descriptive string
 string Truck::displayInfo() const {
-    // Truck brand/model and load
     ostringstream oss;
     oss << "Truck: " << brand << " " << model
         << ", Load " << currentLoad << "/" << capacity << " t";
     return oss.str();
 }
 
+// Max speed decreases with load
 double Truck::maxSpeed() const {
-    // speed drops if heavily loaded
     return 80.0 - currentLoad * 2.0;
 }
 
+// Can we load additional weight?
 bool Truck::canLoad(double weight) const {
     return currentLoad + weight <= capacity;
 }
 
+// Add cargo if possible
 void Truck::load(double weight) {
     if (canLoad(weight)) currentLoad += weight;
 }
 
+// Remove cargo if available
 void Truck::unload(double weight) {
     if (weight <= currentLoad) currentLoad -= weight;
 }
