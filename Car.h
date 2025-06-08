23acba77ff8fa6d78815e94873586ea14317a7f0
LuisHorca <a01713551@tec.mx>
2025-06-07 20:27:48 -0600

    Update Car.h

--- Car.h
+++ Car.h
@@ -10,11 +10,11 @@ class Car : public Vehicle {
 public:
     Car(const string& brand, const string& model, double horsepower);
 
-    string displayInfo() const override;    // info for Car
-    double maxSpeed() const override;       // Car max speed
+    string displayInfo() const override;               // info for Car
+    double maxSpeed() const override;                  // Car max speed
     double travelTime(double distance) const override; // override travelTime
 
-    double fuelConsumptionPerKm() const;    // fuel use per km
+    double fuelConsumptionPerKm() const;               // fuel use per km
     string displayFuelConsumption(double distance) const; // fuel for distance
 
     bool operator==(const Vehicle& other) const override; // compare Cars
