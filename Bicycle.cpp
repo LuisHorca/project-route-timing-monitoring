3273e324b71460591e65620df5ea3090d067665c
LuisHorca <a01713551@tec.mx>
2025-06-10 20:24:54 -0600

    Update Bicycle.cpp

--- Bicycle.cpp
+++ Bicycle.cpp
@@ -5,5 +5,5 @@ Bicycle::Bicycle(const string& brand, const string& model, bool gears)
 {}
 
 double Bicycle::maxSpeed() const {
-    return hasGears ? 30.0 : 15.0; // top speed for bike with gears and not
+    return hasGears ? 30.0 : 15.0; // top speed for bike with gears and not NOTE: The full speed depends on the weight of the user and the kind of terrain. So this only works as a mean of the 2 types of bikes.
 }
