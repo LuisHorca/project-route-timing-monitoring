e03894ad65ad3d6963834198b92a7087c7f4f915
LuisHorca <a01713551@tec.mx>
2025-06-10 20:19:26 -0600

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -5,7 +5,7 @@ Car::Car(const string& brand, const string& model, double hp)
 {}
 
 double Car::maxSpeed() const {
-    return horsepower * 0.3; // speed mutliplier factor per hp
+    return horsepower * 0.3; // speed mutliplier factor per hp NOTE: it doesnt exist a mult factor, but this works as a prediction value.
 }
 
 bool Car::operator==(const Vehicle& other) const {
